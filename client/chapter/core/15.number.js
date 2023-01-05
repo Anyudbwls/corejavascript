70b001bc91bb3d41fb2a731a8b14a4a429955c4a
Yujin <jyj15938@gmail.com>
2023-01-05 14:09:07 +0900

    stirng

--- client/chapter/core/15.number.js
+++ client/chapter/core/15.number.js
@@ -14,7 +14,7 @@ riches = 1e8;
 
 // 그렇다면 아래 작성된 숫자 값은 얼마일까요?
 riches = 1.45e6; // → 1.45 * 10 ** 6
-
+console.log(riches);
 // 작은 수도 `e`를 사용해 표현할 수 있습니다.
 riches = 1e-6; // → 1 / 10 ** 6
 
@@ -23,31 +23,41 @@ riches = 1e-6; // → 1 / 10 ** 6
 let number = 90_127.53100032;
 
 // 내림
-let floor;
-
+let floor = Math.floor(number);
+console.log('floor : ' + floor);
 // 반올림
-let round;
+let round = Math.round(number);
+console.log('round : ' + round);
 
 // 올림
-let ceil;
-
+let ceil = Math.ceil(number);
+console.log('ceil : ' + ceil);
 // 절삭(소수점 이하)
-let truncate;
-
+let truncate = Math.trunc(number);
+console.log('truncate : ' + truncate);
 // 난수
-let random;
+let random = Math.random();
+console.log('random : ' + random);
 
 // 여러 수 중, 최댓값
-let max;
+let max = Math.max(10, 30);
+console.log('max : ' + max);
 
 // 여러 수 중, 최솟값
-let min;
+let min = Math.min(10, 30);
+console.log('min : ' + min);
 
 // 거듭제곱
-let pow;
+let pow = Math.pow(2, 53);
+console.log('pow : ' + pow);
 
 // 최소, 최대 값 사이 난수 반환 함수
-let getRandomMinMax;
+let getRandomMinMax = (a, b) => {
+  if (min > max) throw new Error('최솟값은 최댓값보다 작아야합니다.');
+  return Math.round(Math.random() * (max - min) + min);
+};
+
+console.log(getRandomMinMax(3, 10));
 
 /* 진법 ------------------------------------------------------------------ */
 
