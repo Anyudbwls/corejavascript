54b12964a6a5d536e9df0ff3bc53737600bfbf11
Yujin <jyj15938@gmail.com>
2023-01-16 10:10:30 +0900

    주접 생성기 완성

--- client/main.js
+++ client/main.js
@@ -1,4 +1,5 @@
 import {
+  copy,
   clearContents,
   getInputValue,
   getNode,
@@ -6,6 +7,9 @@ import {
   insertLast,
   typeError,
   isNumericString,
+  showAlert,
+  addClass,
+  removeClass,
 } from './lib/index.js';
 
 import { jujeobData } from './data/data.js';
@@ -28,12 +32,21 @@ function clickSubmitHandler(e) {
 
   if (!name) {
     //error 를 쓰면 그후에도 아예 멈춰 버리기 때문에 alert를 사용
-    alert('TELL ME YOUR NAME !!!!!!!!!');
+    showAlert('.alert-error', 'TELL ME YOUR NAME !!!!!!!!!');
+
+    //GSAP 알아서 지워주고 채워주고
+    //dom 대상위의 트렌스폼을 건드는것
+    gsap.fromTo(resultArea, 0.01, { x: -5 }, { x: 5, clearProps: 'x', repeat: 20 });
+
+    // addClass(resultArea, 'shake');
+    // setTimeout(() => {
+    //   removeClass(resultArea, 'shake');
+    // }, 1000);
     return;
   }
 
   if (isNumericString(name)) {
-    alert('제대로된 이름을 입력해주세요');
+    showAlert('.alert-error', '제대로된 이름을 입력해주세요');
     return;
   }
 
@@ -41,4 +54,15 @@ function clickSubmitHandler(e) {
   insertLast(resultArea, pick);
 }
 
+function clickCopyHandler() {
+  let text = resultArea.textContent;
+
+  // navigator.clipboard.writeText(text);
+  copy(text).then(() => {
+    showAlert('.alert-success', '클립보드에 복사가 완료됐습니다.', 2000);
+  });
+  //약속구문.then(function 실행)
+  //다음에 해야 할 일
+}
 submit.addEventListener('click', clickSubmitHandler);
+resultArea.addEventListener('click', clickCopyHandler);
