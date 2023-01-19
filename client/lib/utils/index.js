bb6e9de6ab9e8e8390325b12dac02e8b9c6c150a
Yujin <jyj15938@gmail.com>
2023-01-19 14:16:29 +0900

    유저 삭제

--- client/lib/utils/index.js
+++ client/lib/utils/index.js
@@ -3,4 +3,5 @@ export * from './memo.js';
 export * from './typeOf.js';
 export * from './xhr.js';
 export * from './delay.js';
-export * from './richi.js';
+export * from './tiger.js';
+export * from './theme.js';
--- client/main.js
+++ client/main.js
@@ -1,28 +1,69 @@
-import { xhrData, insertLast, xhrPromise, tiger } from './lib/index.js';
+/* global gsap */
 
-// xhrPromise
-// .get('https://jsonplaceholder.typicode.com/users/1')
-// .then((res)=>{
-//   insertLast(document.body,JSON.stringify(res));
-// })
-// .catch((err)=>{
-//   console.log(err);
-// })
+import {
+  tiger,
+  delayP,
+  getNode,
+  insertLast,
+  renderEmptyCard,
+  changeColor,
+  renderSpinner,
+  renderUserCard,
+  clearContents,
+  attr,
+} from './lib/index.js';
 
-/* xhrData.get(
-  'https://jsonplaceholder.typicode.com/users/1',
-  (res)=>{
-    insertLast('body',JSON.stringify(res))
-  },
-  (err)=>{
-    insertLast('body','데이터 로딩에 실패했습니다.')
+// rendingUserList 함수 만들기
+// ajax (tiger) get user List
+
+// 유저 카드 생성
+// 생성된 카드로 랜더링
+
+//  1. userList.js로 갑니다.
+//  2. renderUserCard 함수를 만들기
+//  3. 만들어진 함수 안에 createUserCard를 던지고,
+//  4. renderUserCard함수를 사용했을 때  랜더링이 잘 될 수 있도록.
+
+const userCardContainer = getNode('.user-card-inner');
+
+async function rendingUserList() {
+  renderSpinner(userCardContainer);
+
+  try {
+    await delayP(4000);
+
+    getNode('.loadingSpinner').remove();
+
+    let response = await tiger.get('https://jsonplaceholder.typicode.com/users');
+
+    let userData = response.data;
+    // userData.forEach(data=> renderUserCard(userCardContainer,data))
+    userData.forEach((data) => {
+      renderUserCard(userCardContainer, data);
+    });
+
+    changeColor('.user-card');
+
+    gsap.to(gsap.utils.toArray('.user-card'), {
+      x: 0,
+      opacity: 1,
+      duration: 1.5,
+      stagger: 0.2,
+    });
+  } catch (err) {
+    renderEmptyCard(userCardContainer);
   }
-)
-
- */
-async function render() {
-  let response = await tiger.get('https://jsonplaceholder.typicode.com/user/1');
-  console.log(response);
 }
 
-render();
+rendingUserList();
+
+function handler(e) {
+  let deleteBtn = e.target.closest('button');
+  let article = e.target.closest('article');
+
+  if (!deleteBtn || !article) return;
+  let id = attr(article, 'data-index').slice(5);
+
+  tiger.delete(`https://jsonplacehloder.typicode.com/users/${id}`);
+}
+userCardContainer.addEventListener('click', handler);
