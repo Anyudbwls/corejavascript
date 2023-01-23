49a7a7bd4fca3c8ae7bf1a442361e2062ab91460
Yujin <jyj15938@gmail.com>
2023-01-23 16:33:36 +0900

    스토리지 저장

--- client/main.js
+++ client/main.js
@@ -1,1 +1,13 @@
+import { getNode as $ } from './lib/index.js';
+import { saveStorage } from './lib/utils/storage.js';
 
+const textField = $('#textField');
+const deleteButton = $('input[value="삭제"]');
+
+function inputHandler() {
+  saveStorage('area', textField.value);
+}
+
+textField.addEventListener('input', inputHandler);
+
+const hello = '🙇🏻‍♀️';
